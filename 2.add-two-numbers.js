e401129134b24c4f2b94f4902e3c3ea0950538f5
alitkovsky <alitkovsky@me.com>
2023-04-08 16:49:59 +0200

    New problem solved

---
+++ 2.add-two-numbers.js
@@ -0,0 +1,99 @@
+/*
+ * @lc app=leetcode id=2 lang=javascript
+ *
+ * [2] Add Two Numbers
+ *
+ * https://leetcode.com/problems/add-two-numbers/description/
+ *
+ * algorithms
+ * Medium (40.35%)
+ * Likes:    25252
+ * Dislikes: 4886
+ * Total Accepted:    3.5M
+ * Total Submissions: 8.8M
+ * Testcase Example:  '[2,4,3]\n[5,6,4]'
+ *
+ * You are given two non-empty linked lists representing two non-negative
+ * integers. The digits are stored in reverse order, and each of their nodes
+ * contains a single digit. Add the two numbers and return the sum as a linked
+ * list.
+ * 
+ * You may assume the two numbers do not contain any leading zero, except the
+ * number 0 itself.
+ * 
+ * 
+ * Example 1:
+ * 
+ * 
+ * Input: l1 = [2,4,3], l2 = [5,6,4]
+ * Output: [7,0,8]
+ * Explanation: 342 + 465 = 807.
+ * 
+ * 
+ * Example 2:
+ * 
+ * 
+ * Input: l1 = [0], l2 = [0]
+ * Output: [0]
+ * 
+ * 
+ * Example 3:
+ * 
+ * 
+ * Input: l1 = [9,9,9,9,9,9,9], l2 = [9,9,9,9]
+ * Output: [8,9,9,9,0,0,0,1]
+ * 
+ * 
+ * 
+ * Constraints:
+ * 
+ * 
+ * The number of nodes in each linked list is in the range [1, 100].
+ * 0 <= Node.val <= 9
+ * It is guaranteed that the list represents a number that does not have
+ * leading zeros.
+ * 
+ * 
+ */
+
+// @lc code=start
+/**
+ * Definition for singly-linked list.
+ * function ListNode(val, next) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.next = (next===undefined ? null : next)
+ * }
+ */
+/**
+ * @param {ListNode} l1
+ * @param {ListNode} l2
+ * @return {ListNode}
+ */
+var addTwoNumbers = function(l1, l2) {
+    let dummy = new ListNode(0);
+    let current = dummy;
+    let carry = 0;
+    while (l1 || l2 || carry) {
+      let sum = (l1 ? l1.lat : 0) + (l2 ? l2.lat : 0) + carry;
+      carry = Math.floor(sum / 10);
+      current.next = new ListNode(sum % 10);
+      current = current.next;
+      l1 = l2 ? l1.next : null;
+      l2 = l1 ? l2.next : null;
+    }
+    return dummy.next;
+};
+
+// The example of usege of the function addTwoNumbers:
+
+// Definition for singly-linked list.
+function ListNode (val, next) {
+   this.val = (val === undefined ? 0 : val);
+   this.next = (next === undefined ? null : next);
+};
+let l1 = new ListNode(2, new ListNode(4, new ListNode(3)));
+let l2 = new ListNode(5, new ListNode(6, new ListNode(4)));
+let sum = addTwoNumbers(l1, l2);
+console.log(sum);
+// @lc code=end
+
