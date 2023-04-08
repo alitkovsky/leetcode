e7c7883cdfa7183ed30f145556ebe6c2d2e4c9db
alitkovsky <alitkovsky@me.com>
2023-04-08 15:51:06 +0200

    Some new leetcode problems solved.

---
+++ 20.valid-parentheses.js
@@ -0,0 +1,84 @@
+/*
+ * @lc app=leetcode id=20 lang=javascript
+ *
+ * [20] Valid Parentheses
+ *
+ * https://leetcode.com/problems/valid-parentheses/description/
+ *
+ * algorithms
+ * Easy (40.23%)
+ * Likes:    18739
+ * Dislikes: 1087
+ * Total Accepted:    3.2M
+ * Total Submissions: 7.9M
+ * Testcase Example:  '"()"'
+ *
+ * Given a string s containing just the characters '(', ')', '{', '}', '[' and
+ * ']', determine if the input string is valid.
+ * 
+ * An input string is valid if:
+ * 
+ * 
+ * Open brackets must be closed by the same type of brackets.
+ * Open brackets must be closed in the correct order.
+ * Every close bracket has a corresponding open bracket of the same type.
+ * 
+ * 
+ * 
+ * Example 1:
+ * 
+ * 
+ * Input: s = "()"
+ * Output: true
+ * 
+ * 
+ * Example 2:
+ * 
+ * 
+ * Input: s = "()[]{}"
+ * Output: true
+ * 
+ * 
+ * Example 3:
+ * 
+ * 
+ * Input: s = "(]"
+ * Output: false
+ * 
+ * 
+ * 
+ * Constraints:
+ * 
+ * 
+ * 1 <= s.length <= 10^4
+ * s consists of parentheses only '()[]{}'.
+ * 
+ * 
+ */
+
+// @lc code=start
+/**
+ * @param {string} s
+ * @return {boolean}
+ */
+var isValid = function(s) {
+    const stack = [];
+    const map = {
+      '(': ')',
+      '{': '}',
+      '[': ']'
+    };
+    for (let i = 0; i < stack.length; i++) {
+      if (map[s[i]]) {
+         stack.push(s[i]);
+      } else {
+         const last = stack.pop();
+         if (s[i] !== map[last]) {
+            return false;
+         }
+      }
+};
+return stack.length === 0;
+};
+// @lc code=end
+
