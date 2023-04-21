bc88746d33930ed55ef4445e597f7e4e61fe0e80
alitkovsky <alitkovsky@me.com>
2023-04-21 20:21:30 +0200

    New problem solved

---
+++ 21.merge-two-sorted-lists.js
@@ -0,0 +1,104 @@
+/*
+ * @lc app=leetcode id=21 lang=javascript
+ *
+ * [21] Merge Two Sorted Lists
+ *
+ * https://leetcode.com/problems/merge-two-sorted-lists/description/
+ *
+ * algorithms
+ * Easy (62.59%)
+ * Likes:    17965
+ * Dislikes: 1661
+ * Total Accepted:    3.2M
+ * Total Submissions: 5M
+ * Testcase Example:  '[1,2,4]\n[1,3,4]'
+ *
+ * You are given the heads of two sorted linked lists list1 and list2.
+ * 
+ * Merge the two lists in a one sorted list. The list should be made by
+ * splicing together the nodes of the first two lists.
+ * 
+ * Return the head of the merged linked list.
+ * 
+ * 
+ * Example 1:
+ * 
+ * 
+ * Input: list1 = [1,2,4], list2 = [1,3,4]
+ * Output: [1,1,2,3,4,4]
+ * 
+ * 
+ * Example 2:
+ * 
+ * 
+ * Input: list1 = [], list2 = []
+ * Output: []
+ * 
+ * 
+ * Example 3:
+ * 
+ * 
+ * Input: list1 = [], list2 = [0]
+ * Output: [0]
+ * 
+ * 
+ * 
+ * Constraints:
+ * 
+ * 
+ * The number of nodes in both lists is in the range [0, 50].
+ * -100 <= Node.val <= 100
+ * Both list1 and list2 are sorted in non-decreasing order.
+ * 
+ * 
+ */
+
+// @lc code=start
+/**
+ * Definition for singly-linked list.
+ * function ListNode(val, next) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.next = (next===undefined ? null : next)
+ * }
+ */
+/**
+ * @param {ListNode} list1
+ * @param {ListNode} list2
+ * @return {ListNode}
+ */
+const mergeTwoLists = (list1, list2) => {
+    if (list1 === null && list2 === null){
+      return list1;
+    }
+    if (list1 === null){
+      return list2;
+    }
+    if (list2 === null){
+      return list1;
+    }
+
+    let dummy = new ListNode(0);
+    let current = dummy;
+
+    while (list1 && list2){
+      if (list1.val < list2.val){
+         current.next = list1;
+         list1 = list1.next;
+      } else {
+         current.next = list2;
+         list2 = list2.next;
+      }
+      current = current.next;
+
+      if (list1){
+         current.next = list1;
+      }
+      if (list2){
+         current.next = list2
+      }
+    }
+
+    return dummy.next;
+};
+// @lc code=end
+
