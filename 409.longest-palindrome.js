cf4f850455f1e69832f8f108034dce5859fb143c
alitkovsky <alitkovsky@me.com>
2023-04-24 17:18:36 +0200

    A problem 409 solved

---
+++ 409.longest-palindrome.js
@@ -0,0 +1,59 @@
+/*
+ * @lc app=leetcode id=409 lang=javascript
+ *
+ * [409] Longest Palindrome
+ *
+ * https://leetcode.com/problems/longest-palindrome/description/
+ *
+ * algorithms
+ * Easy (54.16%)
+ * Likes:    4619
+ * Dislikes: 286
+ * Total Accepted:    507.8K
+ * Total Submissions: 937.6K
+ * Testcase Example:  '"abccccdd"'
+ *
+ * Given a string s which consists of lowercase or uppercase letters, return
+ * the length of the longest palindrome that can be built with those letters.
+ * 
+ * Letters are case sensitive, for example, "Aa" is not considered a palindrome
+ * here.
+ * 
+ * 
+ * Example 1:
+ * 
+ * 
+ * Input: s = "abccccdd"
+ * Output: 7
+ * Explanation: One longest palindrome that can be built is "dccaccd", whose
+ * length is 7.
+ * 
+ * 
+ * Example 2:
+ * 
+ * 
+ * Input: s = "a"
+ * Output: 1
+ * Explanation: The longest palindrome that can be built is "a", whose length
+ * is 1.
+ * 
+ * 
+ * 
+ * Constraints:
+ * 
+ * 
+ * 1 <= s.length <= 2000
+ * s consists of lowercase and/or uppercase English letters only.
+ * 
+ * 
+ */
+
+// @lc code=start
+/**
+ * @param {string} s
+ * @return {number}
+ */
+const longestPalindrome = (s) => {    
+};
+// @lc code=end
+
